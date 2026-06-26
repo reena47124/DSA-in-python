4c035b67178d65949dd97b3fd1ff81cdb576c092
reena47124 <reenarathode47@gmail.com>
2026-06-26 17:54:31 +0530

    update

---
+++ sliding window/81.py
@@ -0,0 +1,16 @@
+#sliding window
+#find maximum sum of a subarray with k elements,sliding winding technique
+def max_sum(a,k):
+    n=len(a)
+    window_sum=0
+    for i in range(k):
+        window_sum+=a[i]
+    maxsum=window_sum
+    for i in range(k,n):
+        window_sum=window_sum-a[i-k]+a[i]
+        if window_sum>maxsum:
+            maxsum=window_sum
+    return maxsum
+a=[5,2,-1,0,3]
+k=3
+print(max_sum(a,k))           
