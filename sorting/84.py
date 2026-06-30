585c3923cd89c26f4c5cf759f240b8e1775727c2
reena47124 <reenarathode47@gmail.com>
2026-06-30 18:48:37 +0530

    update

---
+++ sorting/84.py
@@ -0,0 +1,14 @@
+#sorting
+#insertion sort
+def insertion_sort(a):
+    n=len(a)
+    for i in range(1,n):
+        key=a[i]
+        j=i-1
+        while j>=0 and a[j]>key:
+            a[j+1]=a[j]
+            j-=1
+        a[j+1]=key
+    return a
+a=[64,25,12,22,11]
+print(insertion_sort(a))        
