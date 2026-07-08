a998edb300f1dd51cdd734b44be17d433d27d1f0
reena47124 <reenarathode47@gmail.com>
2026-07-08 12:48:10 +0530

    update

---
+++ stack/111.py
@@ -0,0 +1,9 @@
+#stack
+#pop operation on stack
+s=[]
+s.append(1)
+s.append(2)
+s.append(3)
+print(f"current element before deletion:{s[-1]}")
+s.pop()
+print(f"current element after deletion:{s[-1]}")
---
+++ stack/112.py
@@ -0,0 +1,12 @@
+#stack
+#isEmpty operation on stack
+s=[]
+if len(s)==0:
+    print("empty")
+else:
+    print("not empty")
+s.append(1)
+if len(s)==0:
+    print("empty")
+else:
+    print("not empty")            
