8a6f7f25747a1beba772253350f28b8931ca930f
reena47124 <reenarathode47@gmail.com>
2026-07-08 12:43:55 +0530

    update

---
+++ stack/109.py
@@ -0,0 +1,7 @@
+#stack
+#push operation on stack
+s=[]
+s.append(1)
+s.append(2)
+s.append(3)
+print(s)
---
+++ stack/110.py
@@ -0,0 +1,7 @@
+#stack
+#top/peek operation on stack
+s=[]
+s.append(1)
+s.append(2)
+s.append(3)
+print(s[-1])
