596929139c5d059f6138a5801e3fcfd0c223b151
reena47124 <reenarathode47@gmail.com>
2026-07-08 14:43:14 +0530

    update

---
+++ stack/114.py
@@ -0,0 +1,21 @@
+#stack
+#push operation on stack
+class Stack:
+    def __init__(self,capacity):
+        self.a=[0]*capacity
+        self.capacity=capacity
+        self.top=-1
+    def push(self,x):
+        if self.top==self.capacity-1:
+            print("stack overflow") 
+            return 
+        self.top+=1
+        self.a[self.top]=x
+s=Stack(5)
+s.push(1)
+s.push(2)
+s.push(3)
+s.push(4)
+s.push(5)
+print(s.a)
+          
